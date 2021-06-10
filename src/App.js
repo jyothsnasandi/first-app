3960edd41ddd18e867677653ad8fda0b548603f7
Jyothsna Sandi <jyothsnas@google.com>
2021-06-10 12:25:52 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from "./Home";
+import About from "./About";
+import Dashboard from "./Dashboard";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import {
+  Navbar,Nav,NavDropdown,Form,FormControl,Button
+
+} from 'react-bootstrap';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+
+<div>
+        <Navbar bg="light" expand="lg">
+  <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+  <Navbar.Toggle aria-controls="navbarScroll" />
+  <Navbar.Collapse id="navbarScroll">
+    <Nav
+      className="mr-auto my-2 my-lg-0"
+      style={{ maxHeight: '100px' }}
+      navbarScroll
+    >
+      <Nav.Link href="#action1">Home</Nav.Link>
+      <Nav.Link href="#action2">Link</Nav.Link>
+      <NavDropdown title="Link" id="navbarScrollingDropdown">
+        <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+        <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
+      </NavDropdown>
+      <Nav.Link href="#" disabled>
+        Link
+      </Nav.Link>
+    </Nav>
+    <Form className="d-flex">
+      <FormControl
+        type="search"
+        placeholder="Search"
+        className="mr-2"
+        aria-label="Search"
+      />
+      <Button variant="outline-success">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+        </div>
+
+      <div>
+        <ul>
+          <li>
+            <Link to="/home">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+
+        
+
+        
+        <Switch>
+          <Route exact path="/home">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+
 }
+
+
 
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,8 @@
+function Home() {
+    return (
+      <h3> Hello Home page
+      </h3>
+    );
+  }
+
+  export default Home;
